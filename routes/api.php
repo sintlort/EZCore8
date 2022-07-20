717cb6f188e600d58a37fcc118990f4a76f28b49
sintlort <61153093+sintlort@users.noreply.github.com>
2022-07-20 22:00:47 +0800

    move handler

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AccountManagement::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');
+    Route::post('/notification/handler/midtrans','notificationHandler');
 
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/user', 'user');
@@ -34,7 +35,6 @@ Route::controller(AccountManagement::class)->group(function () {
         Route::post('/notification/update', 'notificationUpdate');
         Route::post('/receive/fcm', 'receiveFCMToken');
         Route::get('/send/fcm', 'sendNotificationTest');
-        Route::post('/notification/handler/midtrans','notificationHandler');
     });
 });
 
